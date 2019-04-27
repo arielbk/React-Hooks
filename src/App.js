d7d40899bb570a0fb556cecd1790ec90861a529c
arielbk <buchwald.kearney@gmail.com>
2019-04-27 19:12:11 +0200

    Add React-Spring useSpring hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 export default function App() {
   // import custom hooks
@@ -15,7 +16,9 @@ export default function App() {
     setIsLoading(true);
     await fetch(`${API}?amount=1&type=boolean&difficulty=${difficulty}`)
       .then(data => data.json())
-      .then(res => setChallenge(res.results[0]));
+      .then(res => {
+        setChallenge(res.results[0]);
+      });
     setIsLoading(false);
   }
 
@@ -31,7 +34,7 @@ export default function App() {
   return (
     <div className="container">
       <h1 className="title">Hooks Trivia Game</h1>
-      <select class="difficulty-selector" value={difficulty} onChange={e => setDifficulty(e.target.value)}>
+      <select className="difficulty-selector" value={difficulty} onChange={e => setDifficulty(e.target.value)}>
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">hard</option>
@@ -46,22 +49,22 @@ export default function App() {
         && <div className="loading"><div className="loading--spinner" /></div>)
         || (
           !!Object.keys(challenge).length
-          && <div className="question"><h2>Question:</h2><div dangerouslySetInnerHTML={challenge && { __html: challenge.question }} /></div>
+          && <div className="question"><h2>Question:</h2><div dangerouslySetInnerHTML={{ __html: challenge.question }} /></div>
         )}
       {
         Object.keys(challenge).length
           ?
           <>
-            <button className="button button--false" onClick={() => handleAnswer('False')}>False</button>
-            <button className="button button--true" onClick={() => handleAnswer('True')}>True</button>
+            <AnimatedButton cssClass="button button--false" clickHandler={() => handleAnswer('False')}>False</AnimatedButton>
+            <AnimatedButton cssClass="button button--true" clickHandler={() => handleAnswer('True')}>True</AnimatedButton>
           </>
           :
-          <button class="button button--start" onClick={newChallenge}>Start</button>
+          <AnimatedButton cssClass="button button--start" clickHandler={newChallenge}>Start</AnimatedButton>
       }
 
       {didWin !== null && (didWin
-        ? <div class="result result--correct">Last answer was correct! <span role="img" aria-label="congrats emoji">🙌</span></div>
-        : <div class="result result--incorrect">Last answer was incorrect <span role="img" aria-label="thinking emoji">🤔</span></div>
+        ? <div className="result result--correct">Last answer was correct! <span role="img" aria-label="congrats emoji">🙌</span></div>
+        : <div className="result result--incorrect">Last answer was incorrect <span role="img" aria-label="thinking emoji">🤔</span></div>
       )}
     </div>
   );
@@ -113,4 +116,59 @@ function useLocalStorage(key, initialValue) {
   }
 
   return [storedValue, setValue];
+}
+
+// Makes use of React-Springs useSpring hook
+// https://usehooks.com/useSpring/ -- although it seems like there is a problem
+// check out https://www.react-spring.io/docs/hooks/use-spring for the latest
+function AnimatedButton({ children, cssClass, clickHandler }) {
+  // add ref to get elements offset and dimensions
+  const ref = useRef();
+
+  const [isHovered, setHovered] = useState(false);
+
+  // the useSpring hook
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 600, friction: 30 } }));
+
+  return (
+    <animated.button
+      ref={ref}
+      className={cssClass}
+      onClick={clickHandler}
+      onMouseEnter={() => setHovered(true)}
+      onMouseMove={({ clientX, clientY }) => {
+        // get mouse x position within card
+        const x = clientX - (ref.current.offsetLeft - (window.scrollX || window.pageXOffset || document.body.scrollLeft));
+        // get mouse y position within card
+        const y = clientY - (ref.current.offsetTop - (window.scrollY || window.pageYOffset || document.body.scrollTop));
+
+        // set animated vals vased on mouse position and card dimensions
+        const dampen = 5;
+        const xys = [
+          -(y - ref.current.clientHeight / 2) / dampen, // rotateX
+          (x - ref.current.clientWidth / 2) / dampen, // rotateY
+          1.1 // scale
+        ];
+
+        // update values to animate to
+        set({ xys: xys });
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+        // set xys back to default
+        set({ xys: [0, 0, 1] });
+      }}
+      style={{
+        // overlap other cards when it scales up
+        zIndex: isHovered ? 2 : 1,
+        // interpolate values to handle changes
+        transform: props.xys.interpolate(
+          (x, y, s) =>
+            `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+        )
+      }}
+    >
+      {children}
+    </animated.button>
+  );
 }
