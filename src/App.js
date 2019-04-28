a32180c7825182e8346908d3b547ba8bf02691b3
arielbk <buchwald.kearney@gmail.com>
2019-04-28 02:36:16 +0200

    Add onclick for mobile

--- src/App.js
+++ src/App.js
@@ -128,13 +128,35 @@ function AnimatedButton({ children, cssClass, clickHandler }) {
   const [isHovered, setHovered] = useState(false);
 
   // the useSpring hook
-  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 600, friction: 30 } }));
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 700, friction: 20 } }));
 
   return (
     <animated.button
       ref={ref}
       className={cssClass}
-      onClick={clickHandler}
+      onClick={({ clientX, clientY }) => {
+        clickHandler();
+        // get mouse x position within card
+        const x = clientX - (ref.current.offsetLeft - (window.scrollX || window.pageXOffset || document.body.scrollLeft));
+        // get mouse y position within card
+        const y = clientY - (ref.current.offsetTop - (window.scrollY || window.pageYOffset || document.body.scrollTop));
+
+        // set animated vals vased on mouse position and card dimensions
+        const dampen = 5;
+        const xys = [
+          -(y - ref.current.clientHeight / 2) / dampen, // rotateX
+          (x - ref.current.clientWidth / 2) / dampen, // rotateY
+          1.15 // scale
+        ];
+
+        // update values to animate to
+        set({ xys: xys });
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+        // set xys back to default
+        set({ xys: [0, 0, 1] });
+      }}
       onMouseEnter={() => setHovered(true)}
       onMouseMove={({ clientX, clientY }) => {
         // get mouse x position within card
@@ -147,7 +169,7 @@ function AnimatedButton({ children, cssClass, clickHandler }) {
         const xys = [
           -(y - ref.current.clientHeight / 2) / dampen, // rotateX
           (x - ref.current.clientWidth / 2) / dampen, // rotateY
-          1.1 // scale
+          1.15 // scale
         ];
 
         // update values to animate to
@@ -159,12 +181,12 @@ function AnimatedButton({ children, cssClass, clickHandler }) {
         set({ xys: [0, 0, 1] });
       }}
       style={{
-        // overlap other cards when it scales up
-        zIndex: isHovered ? 2 : 1,
+        // // overlap other cards when it scales up
+        // zIndex: isHovered ? 2 : 1,
         // interpolate values to handle changes
         transform: props.xys.interpolate(
           (x, y, s) =>
-            `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+            `perspective(400px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
         )
       }}
     >
